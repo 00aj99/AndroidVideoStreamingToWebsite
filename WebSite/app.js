12e711af40049008969b9f69f382f773d8fc4806
MoiYanes <moi.ses.9319@gmail.com>
2018-07-11 11:05:58 +0000

    Website finished

--- WebSite/app.js
+++ WebSite/app.js
@@ -3,17 +3,19 @@
 var express      = require('express'),
     app          = express(),
     server       = require('http').createServer(app),
-    io           = require('socket.io')(server),
-    fs           = require('fs'),
+    io           = require('socket.io')(server, { wsEngine: 'ws' }),
     port         = process.env.PORT || 3000,
-    request      = require('request'),
     admin        = require("firebase-admin"),
     device_id    = null,
     device_name  = null,
-    numstreaming = 0;
+    url          = null,
+    numstreaming = 0,
+    bodyParser = require('body-parser'),
+    socket_io;
 
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'jade')
 
 
@@ -27,10 +29,9 @@ app.get('/', (req, res) => {
 })
 
 
-
 // -- FIREBASE CONFIGURATION
 // Fetch the service account key JSON file contents
-var serviceAccount = require("./AndroidVideoStreamingUsers-e8b614f9dcca.json"); //AndroidVideoStreamingUsers-e8b614f9dcca / AndroidVideoStreamingUsers-0104103df27a
+var serviceAccount = require("./avsusers-firebase-adminsdk-yy7t7-f4520eb57c.json"); //AndroidVideoStreamingUsers-e8b614f9dcca / AndroidVideoStreamingUsers-0104103df27a
 
 // Initialize the app with a service account, granting admin privileges
 admin.initializeApp({
@@ -39,31 +40,68 @@ admin.initializeApp({
 });
 
 // As an admin, the app has access to read and write all data, regardless of Security Rules
-var db = admin.database();
-var ref = db.ref('/');
-
-
+var db      = admin.database();
+var ref     = db.ref('num_streamings');
+var refr    = db.ref('/');
 
 
 // -- WEBSOCKET
 io.on('connection', function (socket) {
+    socket_io = socket;
 
-    //show database content
-    console.log("\nFirebase data:\nnum_s" + "   " + "device_id" + "          " + "device_name");
+    socket.on('send_mensaje', function(data){
+        refr.update({mensajes: data.mensaje})
+        console.log(data.mensaje);
+    })
+
+    socket.on('load_page', function(){
+        console.log("Pagina recargada, mostrando streamings si los hubiera...")
+        manageDatabase();
+    })
+});
+
+
+manageDatabase();
+function manageDatabase(){
     ref.on("value", function(snapshot) {
-        //console.log(snapshot.val());
-
-        snapshot.forEach(function (devices_data) {
-            devices_data.forEach(function (android_id) {
+        var cont              = 0 ;
+        numstreaming          = snapshot.val();
+        var arr               = new Array();
+        var array_dispostivos = new Array();
+    
+        refr.on("child_added", function(snapshot, prevChildKey) {
+            cont ++;
+            if(cont == 1){
+                snapshot.forEach(function (android_id) {    
+                    device_id   = android_id.val().deviceId;
+                    device_name = android_id.val().deviceName;
+                    url         = android_id.val().url;
+                    arr = [device_id, device_name, url];
+                    array_dispostivos.push(arr);       
+                });
+    
+                if(socket_io != undefined){
+                    console.log("socket_io.emit 1")
+                    socket_io.emit('data_streaming',{'num_streamings': numstreaming, 'dispositivos': array_dispostivos});
+                }
+            }
+        });
+    
+        refr.on("child_removed", function(snapshot) {
+            cont--;
+            console.log("socket_io.emit 2")
+            snapshot.forEach(function (android_id) {    
                 device_id   = android_id.val().deviceId;
                 device_name = android_id.val().deviceName;
+                url         = android_id.val().url;
+                arr = [device_id, device_name, url];
+                array_dispostivos.push(arr);       
             });
-        });
-        
-        numstreaming = snapshot.val().num_streamings;
-        console.log(numstreaming + "       " + device_id + "   " + device_name + "\n");
-
-        socket.broadcast.emit('data_streaming',{'num_streamings': numstreaming, 'device_id': device_id, 'device_name': device_name});
-    });
     
-});
+            if(socket_io != undefined){
+                console.log("socket_io.emit 2")
+                socket_io.emit('data_streaming',{'num_streamings': numstreaming, 'dispositivos': array_dispostivos});
+            }
+        });
+    });
+}
